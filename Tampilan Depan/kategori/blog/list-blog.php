5125471c670b09a5e9fe1c5f7821c4e8dee31de5
Alfred-Nio <nioalfred@gmail.com>
2024-05-07 11:09:16 +0700

    merubah file konten tampilan depan

---
+++ Tampilan Depan/kategori/blog/list-blog.php
@@ -0,0 +1,22 @@
+<?php include("../../layouts/header-depan.php") ?>
+<div class="section search-result-wrap">
+    <div class="container">
+        <h3 class="text-dark mb-4">List Category</h3>
+        <div class="row posts-entry">
+            <div class="col-lg-8">
+                <div class="blog-entry d-flex blog-entry-search-item">
+                    <a href="../../Blog/detail-blog.php" class="img-link me-4">
+                        <img src="../../images/img_1_sq.jpg" alt="Image" class="img-fluid">
+                    </a>
+                    <div>
+                        <span class="date">Apr. 14th, 2022 &bullet; <a href="#">Business</a></span>
+                        <h2><a href="../../Blog/detail-blog.php">Thought you loved Python? Wait until you meet Rust</a></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis ea quis inventore vel voluptas.</p>
+                        <p><a href="../../Blog/detail-blog.php" class="btn btn-sm btn-outline-primary">Read More</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("../../layouts/footer-depan.php") ?>
---
+++ Tampilan Depan/kategori/category.php
@@ -0,0 +1,31 @@
+<?php include("../layouts/header-depan.php")?>
+	<div class="section">
+		<div class="container">
+			<h2 class="posts-entry-title">Categories</h2>
+			<div class="row row-cols-3 g-3">
+				<div class="col">
+					<div class="border border-secondary rounded p-2 text-dark">
+						<h3>Business</h3>
+						<p>Membahas mengenai Bisnis dan Perusahaan</p>
+						<p><a href="../kategori/blog/list-blog.php" class="btn btn-sm btn-outline-primary">Lihat Artikel</a></p>
+					</div>
+				</div>
+				<div class="col">
+					<div class="border border-secondary rounded p-2 text-dark">
+						<h3>Business</h3>
+						<p>Membahas mengenai Bisnis dan Perusahaan</p>
+						<p><a href="../kategori/blog/list-blog.php" class="btn btn-sm btn-outline-primary">Lihat Artikel</a></p>
+					</div>
+				</div>
+				<div class="col">
+					<div class="border border-secondary rounded p-2 text-dark">
+						<h3>Business</h3>
+						<p>Membahas mengenai Bisnis dan Perusahaan</p>
+						<p><a href="../kategori/blog/list-blog.php" class="btn btn-sm btn-outline-primary">Lihat Artikel</a></p>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+<?php include("../layouts/footer-depan.php")?>
